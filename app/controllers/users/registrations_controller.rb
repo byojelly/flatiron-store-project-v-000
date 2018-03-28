92dcfaa83090ee68ed242c0df29e241cea08c032
Alex <github email address>
2018-03-28 06:59:55 -0400

    Done.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :require_logged_in, except: [:new]
+  before_action :require_logged_in, except: [:new, :create]
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
